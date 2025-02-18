dcd1fe71eb4faf1d41b357290853cf0948a64fbf
Serhii Didenko <srg.dd0@gmail.com>
2025-02-18 14:29:51 +0200

    chore: add description

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -5,6 +5,10 @@ const Home = () => {
 				<h1 className='text-5xl md:text-7xl font-bold mb-6 bg-gradient-to-r from-blue-500 to-purple-600 bg-clip-text text-transparent leading-right'>
 					Hi, I&#39;m Serhii
 				</h1>
+				<hr className="h-1 mb-8 max-w-lg mx-auto border-0 bg-gradient-to-r from-blue-500 to-purple-600" />
+				<p className='text-gray-400 text-lg mb-8 max-w-lg mx-auto'>
+					I&#39;m a FullStack developer with a passion for react and node.js. I&#39;m continuously learning and expanding my skill set to become a proficient developer.
+				</p>
 			</div>
 		</section>
 	)
