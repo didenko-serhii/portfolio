67a0a657947f45e505d70752dd82a59a365f44f4
Serhii Didenko <srg.dd0@gmail.com>
2025-02-20 16:04:33 +0200

    chore: fix typo

--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 
 const LoadingScreen = ({ onComplete }) => {
 	const [text, setText] = useState("");
-	const fullText = "<Hello Wolrd />";
+	const fullText = "<Hello World />";
 
 	useEffect(() => {
 		let index = 0;
